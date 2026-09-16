ea580b3103eb078604958516a785bb80d3cc2879
NDP <nguyendinhphu2909@gmail.com>
2026-09-16 14:52:26 +0700

    feat: re-add GoPlay Tech developer apps to portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,8 @@ export const revalidate = 600; // revalidate at most every 10 minutes
 const DEVELOPERS = [
   "Snap Video - Video Downloader",
   "AI+Software+Partner",
-  "Inception+-+AI+Software"
+  "Inception+-+AI+Software",
+  "GoPlay+Tech"
 ];
 
 export default async function Home() {
